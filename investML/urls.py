8932f45e17a266dd10e2d06f52b0ecb81839841a
TEN ALEXANDER <sasha1keshten@gmail.com>
2025-12-21 18:05:37 +0900

    added  login   sign in   and account

--- investML/urls.py
+++ investML/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index,  predict ,  dashboard
+from .views import index,  predict ,  dashboard , sign_up
 
 urlpatterns = [
     path("", index, name="index"),
     path("dashboard/", dashboard, name="dashboard"),
+     path("sign_up/", sign_up, name="sign_up"),
     path("predict/", predict, name="predict"),
 ]
--- investML/views.py
+++ investML/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 import joblib
 from django.http import HttpResponse
 from .ml_model import preprocess_text
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+from django.urls import reverse
+
 
 cv = joblib.load("count_vectorizer.pkl")
 pca = joblib.load("pca.pkl")
@@ -13,6 +18,20 @@ model = joblib.load("logreg_model.pkl")
 def index(request):
     
     return render(request, 'homepage.html')
+
+
+
+def sign_up(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse("dashboard"))
+    else:
+        form = UserCreationForm()
+    return render(request, "sign_up.html", {"form": form})
+    
 
 def dashboard(request):
     return render(request, "users.html")
