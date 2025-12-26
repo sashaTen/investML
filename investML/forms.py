9acc9f7c1ffe3b32215c655db991dd3488d73856
TEN ALEXANDER <sasha1keshten@gmail.com>
2025-12-26 15:48:46 +0900

    chenged the  model  for   portfolio

--- investML/forms.py
+++ investML/forms.py
@@ -1,11 +1,34 @@
 from django import forms
 from .models import Portfolio ,  Tickers
+""" class PortfolioCreateForm(forms.ModelForm):
+    class Meta:
+        model = Portfolio
+        fields = ["name", "total_budget", "risk_tolerance" ] """
+
+
+
+
+
 class PortfolioCreateForm(forms.ModelForm):
     class Meta:
         model = Portfolio
-        fields = ["name", "total_budget", "risk_tolerance" ]
+        fields = [
+            "time_horizon_years",
+            "salary_stability",
+            "average_monthly_income",
+            "investment_percentage",
+            "investing_experience_years",
+           
+        ]
 
-
+        widgets = {
+            "time_horizon_years": forms.NumberInput(attrs={"class": "input"}),
+            "salary_stability": forms.Select(attrs={"class": "input"}),
+            "average_monthly_income": forms.NumberInput(attrs={"class": "input"}),
+            "investment_percentage": forms.NumberInput(attrs={"class": "input"}),
+            "investing_experience_years": forms.NumberInput(attrs={"class": "input"}),
+          
+        }
 
 
 
--- investML/models.py
+++ investML/models.py
@@ -20,7 +20,7 @@ class Tickers(models.Model):
 
 
 
-class   Portfolio(models.Model):
+""" class   Portfolio(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -37,7 +37,61 @@ class   Portfolio(models.Model):
             raise ValidationError("A portfolio can contain at most 10 tickers.")
 
     def __str__(self):
-        return self.name
+        return self.name """
+
+
+
+
+class  Portfolio(models.Model):
+    STABILITY_CHOICES = [
+        (1, "Very unstable"),
+        (2, "Unstable"),
+        (3, "Average"),
+        (4, "Stable"),
+        (5, "Very stable"),
+    ]
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="portfolios"
+    )
+
+    time_horizon_years = models.PositiveIntegerField(
+        help_text="How many years until you need this money"
+    )
+
+    salary_stability = models.IntegerField(
+        choices=STABILITY_CHOICES
+    )
+
+    average_monthly_income = models.FloatField(default=0)
+
+
+    investment_percentage = models.FloatField(
+        help_text="Percentage of savings to invest (0–100)"
+    )
+
+    investing_experience_years = models.IntegerField(
+        help_text="Years of investing experience (0–4)"
+    )
+
+   
+
+    
+    tickers =  models.ManyToManyField(Tickers, related_name='portfolios')
+
+    def __str__(self):
+        return f"{self.user.username} Risk Profile"
+    MAX_TICKERS = 10
+
+    def clean(self):
+        if self.pk and self.tickers.count() > self.MAX_TICKERS:
+            raise ValidationError("A portfolio can contain at most 10 tickers.")
+
+    
+
+
 
 
 
