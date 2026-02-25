5043da98dca7efb8204de2e92579d1fc029352c7
TEN ALEXANDER <sasha1keshten@gmail.com>
2026-02-26 08:43:35 +0900

    deployed the  scripts with   cloud  run which uploads   file to  the   bucket

--- ml_code/retrain.py
+++ ml_code/retrain.py
@@ -1,54 +1,40 @@
 print('hello retrain world')
 #c:/Users/HP/Desktop/investML/ml_code/retrain.py
-
 from google.cloud import storage
 
-def find_blob(bucket_name ):
-    """Lists all the blobs in the bucket."""
-    storage_client = storage.Client()
-    blobs = storage_client.list_blobs(bucket_name)
-
-    print(f"Blobs in bucket '{bucket_name}':")
-    for blob in blobs:
-       
-            print(blob.name)
-            # Return the specific blob if found
-
-# Example usage:
-#find_blob("ml_buckets_a")
 
 
-
-def download_blob(bucket_name, source_blob_name, destination_file_name):
-    """Downloads a blob from the bucket."""
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
     # The ID of your GCS bucket
     # bucket_name = "your-bucket-name"
-
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
     # The ID of your GCS object
-    # source_blob_name = "storage-object-name"
-
-    # The path to which the file should be downloaded
-    # destination_file_name = "local/path/to/file"
+    # destination_blob_name = "storage-object-name"
 
     storage_client = storage.Client()
-
     bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
 
-    # Construct a client side representation of a blob.
-    # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve
-    # any content from Google Cloud Storage. As we don't need additional data,
-    # using `Bucket.blob` is preferred here.
-    blob = bucket.blob(source_blob_name)
-    blob.download_to_filename(destination_file_name)
+    # Optional: set a generation-match precondition to avoid potential race conditions
+    # and data corruptions. The request to upload is aborted if the object's
+    # generation number does not match your precondition. For a destination
+    # object that does not yet exist, set the if_generation_match precondition to 0.
+    # If the destination object already exists in your bucket, set instead a
+    # generation-match precondition using its generation number.
+    generation_match_precondition = 0
+
+    blob.upload_from_filename(source_file_name, if_generation_match=generation_match_precondition)
 
     print(
-        "Downloaded storage object {} from bucket {} to local file {}.".format(
-            source_blob_name, bucket_name, destination_file_name
-        )
+        f"File {source_file_name} uploaded to {destination_blob_name}."
     )
 
 
-#download_blob("ml_buckets_a", "knn_model.pkl", r"C:\Users\HP\Desktop\investML\ml_artifacts\knn_model.pkl") 
-#download_blob("ml_buckets_a", "knn_count_vectorizer.pkl", r"C:\Users\HP\Desktop\investML\ml_artifacts\knn_count_vectorizer.pkl") 
-#download_blob("ml_buckets_a", "knn_pca.pkl", r"C:\Users\HP\Desktop\investML\ml_artifacts\knn_pca.pkl") 
 
+if __name__ == "__main__":
+    bucket_name = "ml_buckets_a"
+    source_file_name =  "knn_pca.pkl"
+    destination_blob_name = "knn_pca_v3.pkl"
+    upload_blob(bucket_name, source_file_name, destination_blob_name)
