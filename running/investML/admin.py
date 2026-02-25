c362afd32c8573ab5579eafbbd9012f2e2353609
TEN ALEXANDER <sasha1keshten@gmail.com>
2026-02-26 05:09:17 +0900

    again   gcloud  and deleted the    running  folder

--- running/investML/admin.py
+++
@@ -1,6 +0,0 @@
-
-from django.contrib import admin
-from .models import Tickers ,  Portfolio
-
-admin.site.register(Tickers )
-admin.site.register(Portfolio)
--- running/investML/miscelan.py
+++
@@ -1,2 +0,0 @@
-def     print_any(word):
-    print(word)
