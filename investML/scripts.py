bafbe740c6b6595420ad9077f724f6eace424c6a
TEN ALEXANDER <sasha1keshten@gmail.com>
2026-03-15 12:55:23 +0900

    test    the system

--- investML/scripts.py
+++ investML/scripts.py
@@ -306,7 +306,7 @@ def  get_ticker_news(ticker):
             sentiment_text=response["results"][0]["content"]  # Assuming you have the sentiment text available
         )
     \
-    if Sentiment.objects.count() %1000== 0 :
+    if Sentiment.objects.count() > 0 :
         current_data_version = find_latest_model_version(
     ARTIFACTS_DIR,
     r"data"
