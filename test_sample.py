13413021fef7fce05f66dacbedb6298c3a7f3345
TEN ALEXANDER <sasha1keshten@gmail.com>
2026-03-20 13:26:05 +0900

    deleted    unused  files

--- test_sample.py
+++ test_sample.py
@@ -1,17 +1,10 @@
-#     pytest  -m   unit
-from investML.miscelan import print_any 
+
 from  ml_code.testing_ml  import load_df
 import pytest
 import pandas as pd
 
 
 
-@pytest.mark.unit
-def test_print_any():
-
-    test_word = "Hello, InvestML!"
-    print_any(test_word)  # Just ensure it runs without error
-    assert True  # If no exception, the test passes
 
 
 
