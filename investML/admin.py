41d20682ae53968b5d8a4780e73ac79a70b53420
Alexandra Kim <alexandrakim@MacBook-Air-Alexandra.local>
2026-05-30 07:29:11 +0900

    test

--- investML/admin.py
+++ investML/admin.py
@@ -1,7 +1,5 @@
 
 from django.contrib import admin
-from .models import Tickers ,  Portfolio , RiskToleranceProfile
-
+from .models import Tickers ,  Portfolio 
 admin.site.register(Tickers )
 admin.site.register(Portfolio)
-admin.site.register(RiskToleranceProfile)
