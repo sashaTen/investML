180419f7fd07f063e75a8613450d215be432f121
Alexandra Kim <alexandrakim@MacBook-Air-Alexandra.local>
2026-06-10 04:08:42 +0900

    no debug

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "invest-app-213061868662.asia-northeast3.run.app",
